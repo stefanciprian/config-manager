c35858f555b3f44efa0761b1e9e5d0c7a90d70a4
Stefan <hotoleanu.ciprian@gmail.com>
2023-02-19 09:28:35 +0200

    Fastify host for Heroku

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ void (async () => {
   appService.enableOpenApi(app);
 
   await app.register(fastifyMultipart);
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
   appService.logOnServerStart(appFactory, configFactory);
 })();
